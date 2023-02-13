69e99568453be42a4f885882546b6b7277c484c5
alexZholudev <alexZholudev222@gmail.com>
2023-02-13 13:05:11 +0200

    added 3

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Main = () => {
     return (
         <div>
             <h1>Main</h1>
-            <NavLink to={"/products"}>
-                <h2>products</h2>
-            </NavLink>
+            <Link to="/products">
+                <h2 className="text-emerald-50">products</h2>
+            </Link>
         </div>
     );
 };
